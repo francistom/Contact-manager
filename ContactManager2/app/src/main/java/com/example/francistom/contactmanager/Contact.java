40a29975aa35ae329c488716c1179fb5654a9d9f
Francis Tom <Francis Tom>
2017-08-28 23:46:23 +0530

    Added map functionality

---
+++ ContactManager2/app/src/main/java/com/example/francistom/contactmanager/Contact.java
@@ -0,0 +1,20 @@
+package com.example.francistom.contactmanager;
+
+/**
+ * Created by Francis Tom on 28-08-2017.
+ */
+
+public class Contact {
+    String name;
+    String email;
+    String mobile;
+    String home;
+
+    public Contact(String n, String e, String m, String h)
+    {
+        this.name = n;
+        this.email = e;
+        this.mobile = m;
+        this.home = h;
+    }
+}
